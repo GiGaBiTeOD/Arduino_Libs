a72fcab7144e7bb763dc7caf4f91af8a08fac644
GiGaBiTeOD <glgabiteod@gmail.com>
2017-10-24 21:18:19 +0300

    added test sketch

---
+++ Sketch1/Sketch1/Debug/Sketch1.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\Oleg\AppData\Local\Temp\VMBuilds\Sketch1\mega_atmega2560\Debug\Sketch1.cpp.o: \
+ C:\Users\Oleg\AppData\Local\Temp\VMBuilds\Sketch1\mega_atmega2560\Debug\Sketch1.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Adafruit-MCP23017-Arduino-Library-master/Adafruit_MCP23017.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\53ggn0bh.0i3\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
---
+++ Sketch1/Sketch1/Sketch1.ino
@@ -0,0 +1,66 @@
+/*
+ Name:		Sketch1.ino
+ Created:	10/21/2017 2:16:12 PM
+ Author:	Oleg
+*/
+
+#include <Wire.h>
+#include "Adafruit_MCP23017.h"
+
+Adafruit_MCP23017 mcp_IN;
+Adafruit_MCP23017 mcp_OUT;
+
+int IN0_val, IN1_val, IN2_val, OUT0_val, serial_val;
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+	Serial.println("---SETUP START---");
+	Serial.begin(9600);
+
+	mcp_IN.begin(0);
+	mcp_IN.pinMode(0, INPUT); //21
+	mcp_IN.pinMode(1, INPUT); //22
+	mcp_IN.pinMode(2, INPUT); //23
+	//mcp_IN.pullUp(0, HIGH);  // turn on a 100K pullup internally
+
+	mcp_OUT.begin(1);
+	mcp_OUT.pinMode(0, OUTPUT);
+
+	Serial.println("---SETUP END---");
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+	
+	if (Serial.available())
+	{
+		serial_val = Serial.read();
+		//Serial.write(serial_val);
+	}
+	
+	IN0_val = mcp_IN.digitalRead(0);
+	IN1_val = mcp_IN.digitalRead(1);
+	IN2_val = mcp_IN.digitalRead(2);
+
+	//_MCP2307_1_O_IO1 = ((((((_MCP2307_0_O_IO9) ^ (_MCP2307_0_O_IO10))) ^ (_MCP2307_0_O_IO11))) ^ (_gtv1));
+	OUT0_val = ((((((IN0_val) ^ (IN1_val))) ^ (IN2_val))) ^ (serial_val));
+	//OUT0_val = ((((IN0_val) ^ (IN1_val))) ^ (IN2_val));
+	//OUT0_val = IN0_val;
+
+
+	mcp_OUT.digitalWrite(0, OUT0_val);
+	/*
+	Serial.println(IN0_val, DEC);
+	Serial.println(IN1_val, DEC);
+	Serial.println(IN2_val, DEC);
+
+	Serial.println(OUT0_val, DEC);
+
+	mcp_OUT.digitalWrite(1, HIGH);
+	mcp_OUT.digitalWrite(7, LOW);
+	Serial.println("=========================== LOOP ======================");	
+	delay(1000);
+	*/
+}

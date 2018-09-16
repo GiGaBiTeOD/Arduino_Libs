7d19de8f525201fe2377ac6c440cc8f7b4fb640c
Oleg <glgabiteod@gmail.com>
2018-09-17 00:25:08 +0300

    add giga unit src

---
+++ GiGaUnit/GiGaUnit.ino
@@ -0,0 +1,63 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       GiGaUnit.ino
+    Created:	9/16/2018 11:00:08 PM
+    Author:     DESKTOP-H0VFUKT\prosp
+*/
+
+// Define User Types below here or use a .h file
+#define PIN_KEYBOARD_ANALOG 2
+#define PIN_LIGHTSENSOR_ANALOG 1
+#define PIN_AIR_ANALOG 0
+
+#define PIN_MOTION_DIGITAL 3
+#define PIN_DHT22_DIGITAL 4
+#define PIN_AIR_DIGITAL 5
+#define PIN_IR_DIGITAL 7
+#define PIN_433_TRANS_DIGITAL 8
+#define PIN_433_RES_DIGITAL 9
+#define PIN_315_TRANS_DIGITAL 10
+#define PIN_315_RES_DIGITAL 11
+#define PIN_WATER_C_FLOW_DIGITAL 18
+#define PIN_WATER_H_FLOW_DIGITAL 19
+#define PIN_WATER_C_TEMP_DIGITAL 22
+#define PIN_WATER_H_TEMP_DIGITAL 23
+
+#define PIN_SDA 20
+#define PIN_SCL 21
+
+#define PIN_SPI_MISO_DIGITAL 50
+#define PIN_SPI_MOSI_DIGITAL 51
+#define PIN_SPI_SCK_DIGITAL 52
+
+#define PIN_SPI_MISO_NRF_DIGITAL 50
+#define PIN_SPI_MOSI_NRF_DIGITAL 51
+#define PIN_SPI_SCK_NRF_DIGITAL 52
+#define PIN_SPI_CE_NRF_DIGITAL 6
+#define PIN_SPI_CS_MRF_DIGITAL 53
+
+
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
